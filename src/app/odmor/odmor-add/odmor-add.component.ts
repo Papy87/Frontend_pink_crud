fd8bffd03003bb8a4a158409f34fc72fca8cab23
Katarina <kacajaksic@outlook.com>
2019-09-13 11:32:45 +0200

    third commit

--- src/app/odmor/odmor-add/odmor-add.component.ts
+++ src/app/odmor/odmor-add/odmor-add.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { DataStorageService } from "src/app/data.storage.service";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
-  selector: 'app-odmor-add',
-  templateUrl: './odmor-add.component.html',
-  styleUrls: ['./odmor-add.component.css']
+  selector: "app-odmor-add",
+  templateUrl: "./odmor-add.component.html",
+  styleUrls: ["./odmor-add.component.css"]
 })
 export class OdmorAddComponent implements OnInit {
+  constructor(private dataStorage: DataStorageService, private router: Router) {}
 
-  constructor() { }
+  public odmorForm: FormGroup;
 
   ngOnInit() {
+    this.initForm()
+  }
+  private initForm() {
+    this.odmorForm = new FormGroup({
+      sezona: new FormControl("", [Validators.required]),
+      datumPocetka: new FormControl("", [Validators.required]),
+      radnihdana: new FormControl("", [Validators.required]),
+      napomena: new FormControl("", [Validators.required])
+    });
   }
 
+  onSubmit(): void {
+    console.log(this.odmorForm.value);
+    this.dataStorage.insertOdmor(this.odmorForm.value).subscribe(odmor => {
+      this.router.navigate(["odmor/list"]);
+    });
+  }
 }
--- src/app/odmor/odmor.module.ts
+++ src/app/odmor/odmor.module.ts
@@ -1,29 +1,23 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { OdmorComponent } from './odmor.component';
-import { OdmorAddComponent } from './odmor-add/odmor-add.component';
-import { OdmorEditComponent } from './odmor-edit/odmor-edit.component';
-import { OdmorListaComponent } from './odmor-lista/odmor-lista.component';
-import { OdmorRoutingModule } from './odmor.routing.module';
-import { OdmorShowComponent } from './odmor-show/odmor-show.component';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { OdmorComponent } from "./odmor.component";
+import { OdmorAddComponent } from "./odmor-add/odmor-add.component";
+import { OdmorEditComponent } from "./odmor-edit/odmor-edit.component";
+import { OdmorListaComponent } from "./odmor-lista/odmor-lista.component";
+import { OdmorRoutingModule } from "./odmor.routing.module";
+import { OdmorShowComponent } from "./odmor-show/odmor-show.component";
 
 @NgModule({
   declarations: [
-OdmorComponent,
-OdmorAddComponent,
-OdmorEditComponent,
-OdmorListaComponent,
-OdmorAddComponent,
-OdmorShowComponent
-],
-  exports:[
- ],
-  imports: [
-    CommonModule,
-    FormsModule,
-    OdmorRoutingModule
-   
-  ]
+    OdmorComponent,
+    OdmorAddComponent,
+    OdmorEditComponent,
+    OdmorListaComponent,
+    OdmorAddComponent,
+    OdmorShowComponent
+  ],
+  exports: [],
+  imports: [CommonModule, FormsModule, OdmorRoutingModule, ReactiveFormsModule]
 })
 export class OdmorModule {}
